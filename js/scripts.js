11004b1cb6ed2fe765cb6ab47d20ca0c9e5ba6ec
Jon <roomcleaner@protonmail.org>
2019-03-30 23:16:12 -0500

    JS for buttons / input, add button values

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,33 @@
 "use strict";
 
-function plus() {
+let histList = [];
+let active = document.querySelector("#active");
 
+
+function operate(expression) {
+  let value;
+
+  return value;
 }
 
-window.onload = () => {
-  window.addEventListener('keydown', function() {
+function addKeypress(keyPress) {
+  if (keyPress === "clr") {
+    active.textContent = "";
+  } else if (keyPress === "del") {
+    active.textContent = active.textContent.slice(0,active.textContent.length-1);
+  } else if (keyPress === "=") {
+    active.textContent = operate(active.textContent);
+  }
+  else {
+    active.textContent += keyPress;
+  }
+}
 
+let buttons = document.querySelector(".buttons").childNodes.forEach(button => {
+  button.addEventListener("mousedown", () => button.classList.add("active"));
+  button.addEventListener("mouseup", () => {
+    button.classList.remove("active");
+    addKeypress(button.value);
   });
-};
+});
+
