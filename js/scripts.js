66294018239c89048535d5ae18a001fcd13dd3c2
Jon <roomcleaner@protonmail.com>
2019-04-01 16:12:16 -0500

    buttons / event listeners refactored, history / operate implemented

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,64 @@
 "use strict";
 
-let histList = [];
+let histList = document.querySelector(".history");
 let active = document.querySelector("#active");
-
+let warn = document.querySelector("#warn");
 
 function operate(expression) {
   let value;
+  try {
+    warn.textContent = "";
+    value = Function('"use strict";return('+expression+')')();
+    addHistory(expression, value);
+    return value;
+  } catch (e) {
+    warn.textContent = "malformed input";
+    return expression;
+  }
+}
 
-  return value;
+function addHistory(expression, value) {
+  histList.innerHTML = `<div class="hist-elem"><span class="left">`+expression+`</span><span class="right"><span>=</span><span>`+value+`</span></span></div>`+histList.innerHTML;
 }
 
 function addKeypress(keyPress) {
-  if (keyPress === "clr") {
-    active.textContent = "";
-  } else if (keyPress === "del") {
-    active.textContent = active.textContent.slice(0,active.textContent.length-1);
-  } else if (keyPress === "=") {
-    active.textContent = operate(active.textContent);
+  // TODO: insert character depending on the current selection area.
+  if (keyPress === "c" || keyPress === "C") {
+    active.value = "";
+  } else if (keyPress === "Backspace") {
+    active.value = active.value.slice(0,active.value.length-1);
+  } else if (keyPress === "=" || keyPress === "Enter") {
+    active.value = operate(active.value);
   }
   else {
-    active.textContent += keyPress;
+    active.value += keyPress;
   }
 }
 
-let buttons = document.querySelector(".buttons").childNodes.forEach(button => {
-  button.addEventListener("mousedown", () => button.classList.add("active"));
-  button.addEventListener("mouseup", () => {
-    button.classList.remove("active");
-    addKeypress(button.value);
-  });
+let buttons = document.querySelectorAll(".number, .operator");
+document.addEventListener("click", () => active.focus());
+document.querySelector("#clear").addEventListener("dblclick", () => {
+  while (histList.firstChild) {
+    histList.removeChild(histList.firstChild);
+  }
 });
 
+// TODO: expand this into separate buttons maybe so there's no constant checking of unnecessary boolean expressions
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    addKeypress(button.value);
+  });
+  document.addEventListener("keydown", (e) => {
+    if (e.key !== "c" && (button.value === e.key || e.key === "Enter" && button.value === "=")) {
+      button.classList.add("active");
+    }
+    if (e.key === "Enter" && button.value === "=") {
+      addKeypress(e.key);
+    }
+  });
+  document.addEventListener("keyup", (e) => {
+    if (e.key !== "c" && (button.value === e.key || e.key === "Enter" && button.value === "=")) {
+      button.classList.remove("active");
+    }
+  });
+});
